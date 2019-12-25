9737d66138a56aa074d57a7b0c74a84d078f63fd
Yaroslav Dobzhanskij <merlyn.tgz@gmail.com>
2019-12-25 02:46:16 +0200

    initial release v1.0.0-beta.1

---
+++ index.js
@@ -0,0 +1,33 @@
+module.exports = {
+  getKey: target => target._id,
+  getCondition: target => target._id,
+  getStream: (source, condition) => source.find(condition).lean().cursor(),
+  getDataHandler: ({ targets, extractKey, isMultiple, targetField, sourceField, assignData }) => (foreign) => {
+    const target = targets[extractKey(foreign)];
+
+    if (assignData) return assignData(target, foreign);
+    if (!isMultiple) target[targetField] = foreign[sourceField];
+    else (target[targetField] = target[targetField] || []).push(foreign[sourceField]);
+  },
+  getAddingMethod: ({
+    targets, getKey, getCondition, defaultValue, targetField, condition, foreignField, inner,
+  }) => {
+    let isConditionSetup;
+
+    return (target) => {
+      Object.assign(targets, { [getKey(target)]: target });
+      const itemCondition = getCondition(target);
+
+      if (defaultValue) if (typeof defaultValue !== 'object') target[targetField] = defaultValue;
+      else Object.assign(target, defaultValue);
+      if (!isConditionSetup) {
+        if (typeof itemCondition === 'object') condition.$or = condition.$or || inner;
+        else condition[foreignField] = condition[foreignField] || { $in: inner };
+
+        isConditionSetup = true;
+      }
+
+      inner.push(itemCondition);
+    };
+  },
+};
